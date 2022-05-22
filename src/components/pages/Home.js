7cb44d48867e68ae5cb15454a2d9ea7c36efea3e
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-05-22 14:42:34 +0100

    Synced the region and search field.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,13 +6,14 @@ const Home = () => {
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(false);
   const [value, setValue] = useState("");
-  let [filteredCountries, setFilteredCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [regionCountries, setRegionCountries] = useState({});
 
   useEffect(() => {
     async function getData() {
       try {
         const response = await fetch("https://restcountries.com/v2/all");
-        if (!response.ok) throw "Could not fetch countries";
+        if (!response.ok) throw new Error("Could not fetch countries");
         const data = await response.json();
         const usefulData = data.map((country) => {
           const { name, population, region, capital, flags } = country;
@@ -38,14 +39,17 @@ const Home = () => {
   function handleChange(e) {
     setValue(e.target.value);
   }
+
   function handleDisplayRegion(e) {
     const region = e.target.textContent;
     const regionText = document.getElementById("region-text");
     const el = e.target.tagName;
     if (region === "All") {
       setFilteredCountries(countries);
+      setRegionCountries({ text: "Filter by Region", country: countries });
       regionText.textContent = "Filter by Region";
       hideRegion();
+      setValue("");
       return;
     }
     if (el === "BUTTON") {
@@ -54,22 +58,26 @@ const Home = () => {
         if (country.region === region) regionCountries.push(country);
       });
       setFilteredCountries(regionCountries);
+      setRegionCountries({ text: region, country: regionCountries });
       regionText.textContent = region;
       hideRegion();
+      setValue("");
     }
   }
 
   function handleSearch(e) {
     let searchCountry = e.target.value;
+    const regCountries = { ...regionCountries };
     if (searchCountry.length === 0) {
-      setFilteredCountries(countries);
-      document.getElementById("region-text").textContent = "Filter by Region";
+      setFilteredCountries(regCountries.country);
+      document.getElementById("region-text").textContent = regCountries.text;
       return;
     }
-    const tempCountries = filteredCountries.filter((country) =>
+    const tempCountries = regCountries.country.filter((country) =>
       country.name.toLowerCase().includes(searchCountry.toLowerCase())
     );
     setFilteredCountries(tempCountries);
+    // console.log(tempCountries, filteredCountries);
   }
 
   function clearSearchInput(e) {
